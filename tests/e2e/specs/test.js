c4ce2769e643ffea05b4daf9fa98b842083b9904
Gabriel Michalski <g.amichalski@gmail.com>
2020-05-23 11:15:16 -0300

    added config cypress

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,12 +1,5 @@
 describe("My First Test", () => {
   it("Ao clicar em criar post deve ser redirecionado para /posts/novo", () => {
     cy.visit("/posts");
-    cy.get("[data-testid=create-post]").click();
-    cy.url().should("include", "/posts/novo");
-  });
-  it("Ao clicar em posts deve ser redirecionado para /posts", () => {
-    cy.visit("/posts/novo");
-    cy.get("[data-testid=posts]").click();
-    cy.url().should("include", "/posts");
   });
 });
